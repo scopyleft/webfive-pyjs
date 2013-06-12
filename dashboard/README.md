b0eb732b967e3f36d6df0b2626d2851d5dba5341
Vincent Agnano <vincent.agnano@scopyleft.fr>
2013-06-12 15:23:30 +0200

    added dashboard in draft status

---
+++ dashboard/README.md
@@ -0,0 +1,22 @@
+# Tornado + Polymer for web5 conference
+
+**Rendering a CPU graph through websockets using Shadow DOM elements.**
+
+`Tornado` (Python) is serving data retrieved by `psutil` (Python) via websockets.
+`gRahaeljs` (JavaScript) is drawing a line graph of this data updated via 
+`Polymer` (JavaScript) and rendered through reusable Shadow DOM elements.
+
+## Usage
+
+    $ pip install -r requirements.txt
+    $ git clone git://github.com/Polymer/polymer.git --recursive static/polymer
+    $ python server.py
+    go to http://localhost:8888/
+    click the button and enjoy!
+
+## Ressources
+
+* http://www.polymer-project.org/
+* http://www.tornadoweb.org/en/stable/
+* http://g.raphaeljs.com/
+* https://pypi.python.org/pypi/psutil/
---
+++ dashboard/server.py
@@ -0,0 +1,70 @@
+import os
+import sys
+import json
+import time
+
+import psutil
+
+from tornado import httpserver, ioloop, web, websocket
+
+
+SERVER_PATH = os.path.abspath(os.path.dirname(__file__))
+STATIC_PATH = os.path.join(SERVER_PATH, 'static')
+
+settings = {
+    "static_path": STATIC_PATH,
+    "server_name": "localhost",
+    "debug": True,
+}
+
+connections = []
+
+
+class MainHandler(web.RequestHandler):
+
+    def get(self):
+        self.render("static/templates/index.html",
+                    server_name=settings['server_name'])
+
+
+class GlobalCPUWSHandler(websocket.WebSocketHandler):
+
+    def on_message(self, message):
+        for i in range(21):
+            data = {
+                'x': i,
+                'y': psutil.cpu_percent(interval=1)
+            }
+            self.write_message(json.dumps(data))
+            time.sleep(1)
+
+
+class FirefoxCPUWSHandler(websocket.WebSocketHandler):
+
+    def on_message(self, message):
+        firefox_pid = [pid for pid in psutil.get_pid_list()
+                       if psutil.Process(pid).name == "firefox"][0]
+        firefox_process = psutil.Process(firefox_pid)
+        for i in range(21):
+            data = {
+                'x': i,
+                'y': firefox_process.get_cpu_percent(interval=1.0)
+            }
+            self.write_message(json.dumps(data))
+            time.sleep(1)
+
+
+application = web.Application([
+    (r'/', MainHandler),
+    (r'/cpu/global', GlobalCPUWSHandler),
+    (r'/cpu/firefox', FirefoxCPUWSHandler),
+    (r"/", web.StaticFileHandler,
+        dict(path=settings['static_path'])),
+], **settings)
+
+if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        settings['server_name'] = sys.argv[1]
+    http_server = httpserver.HTTPServer(application)
+    http_server.listen(8888)
+    ioloop.IOLoop.instance().start()
